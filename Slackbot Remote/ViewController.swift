42d0124a4fce72d920d29c30665d849573e7c249
Rogier Bom <rogier@rogierbom.nl>
2015-12-22 17:30:00 +0100

    Initial Commit

---
+++ Slackbot Remote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slackbot Remote
+//
+//  Created by Rogier Bom on 22/12/15.
+//  Copyright © 2015 Rogier Bom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
